142641bd70f5de5f0f722ce251a00bbabf6b3da9
Shiva7781 <shivavishwakarma7781@gmail.com>
2023-03-10 01:12:10 +0530

    HamBurger Search

--- hamburger/src/components/Navbar.jsx
+++ hamburger/src/components/Navbar.jsx
@@ -4,24 +4,48 @@ import Burger from "./Burger";
 import logo from "./logo.png";
 
 const Nav = styled.nav`
+  width: 100%;
   height: 55px;
   border-bottom: 3px solid #f1f1f1;
   padding: 0 3px 0 0;
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media (max-width: 768px) {
+    justify-content: start;
+  }
 `;
 
 const Img = styled.img`
   width: 333px;
-  /* border: 1px solid red; */
+  height: 77px;
+
+  @media (max-width: 768px) {
+    /* display: none; */
+
+    width: 55px;
+    height: 55px;
+    margin: 1rem;
+  }
+`;
+
+const Input = styled.input`
+  font-size: 16px;
+  outline: none;
+  min-width: 40%;
+  padding: 11px;
+
+  @media (max-width: 768px) {
+    margin: 1rem;
+  }
 `;
 
 const Navbar = () => {
   return (
     <Nav>
       <Img src={logo} />
-
+      <Input placeholder="Search for products, brand and more" />
       <Burger />
     </Nav>
   );
